34726c829682c88afaa72d716f566a41e921fafd
kkumtree <bc0569@naver.com>
2021-01-15 21:35:37 +0900

    new baristar is generated

---
+++ 210115-the-world/src/com/example/helloworld/TheWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+/*
+**  first study about Java in 210115
+**  How to Run? Ctrl+Alt+R
+ */
+
+public class TheWorld {
+    public static void main(String[] args){
+        System.out.println("TheWorld!");
+    }
+}
